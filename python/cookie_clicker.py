feacd485f04693dc7e48a76aba9ae3a1f8016f18
swanito68 <swan.broquereau@hotmail.com>
2025-05-28 12:59:08 +0000

    Update cookie_clicker.py

--- python/cookie_clicker.py
+++ python/cookie_clicker.py
@@ -34,10 +34,10 @@ def upgrade_shop():
     global cookies
     clear()
     while True:
-        print("===== UPGRADE STORE =====")
+        print("============== UPGRADE STORE ==============")
         for key, value in upgrade_store.items():
             print(f"{key}: {value:10}")
-        print("=========================")
+        print("===========================================")
         print(f"You have {cookies} cookies")
         choice = input("Choose an upgrade (or press Enter to exit): ").strip()
         if choice == "":
